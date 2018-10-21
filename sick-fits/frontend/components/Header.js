be94dc03631ed5bb36fe9cf8d37cf05522282b41
myvagrantlife <edward@myvagrantlife>
2018-10-21 17:57:51 +0200

    setup eslint and prettier

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -2,11 +2,11 @@ import Nav from './Nav'
 
 export default () => (
   <div>
-    <div className='bar'>
-      <a href=''>Sick Fits</a>
+    <div className="bar">
+      <a href="">Sick Fits</a>
       <Nav />
     </div>
-    <div className='sub-bar'>
+    <div className="sub-bar">
       <p>Search</p>
     </div>
     <div>Cart</div>
